1634a6a6f8584dbe8019f511e097ba3d93ebf242
Narendiran Chembu <nchembu@test3.irc.sara.nl.irc.sara.nl>
2020-01-24 16:35:28 +0100

    short partitions for debug

--- domain-adaptation/finetuning_bert_enron/job.sh
+++ domain-adaptation/finetuning_bert_enron/job.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=finetune_bert_enron
-#SBATCH --partition=short
+#SBATCH --partition=gpu_short
 #SBATCH --time=5:00
-#SBATCH --mail-type=ALL
-#SBATCH --mail-user=cgnarendiran@gmail.com
 
 python ../run_lm_finetuning.py  \
     --output_dir='output/' \
@@ -11,4 +9,6 @@ python ../run_lm_finetuning.py  \
     --model_name_or_path=bert-base-cased \
     --do_train \
     --train_data_file='../enron_lm.csv' \
-    --mlm
+    --mlm \
+    --overwrite_cache \
+    --overwrite_output_dir
