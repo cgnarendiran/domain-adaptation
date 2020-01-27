598578be40ac832b5f8025beca9fa64f6a2e2bce
NILOIDE <nilstoltanso@gmail.com>
2020-01-27 17:05:44 +0100

    minor error corrections

--- domain-adaptation/run_lm_finetuning_tsv.py
+++ domain-adaptation/run_lm_finetuning_tsv.py
@@ -182,7 +182,7 @@ class TextDataset(Dataset):
 
             logger.info("Saving features into cached file %s", cached_features_file)
             with open(cached_features_file, 'wb') as handle:
-                pickle.dump(self.complete_inputs, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                pickle.dump(self.examples, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     def __len__(self):
         return len(self.examples)
@@ -357,11 +357,11 @@ def train(args, train_dataset, model, tokenizer):
             batch_inputs, attentions = batch
             # print(batch_inputs.shape)
             # print(batch_inputs.dtype)
-            print(batch_inputs)
-            print(attentions)
+            # print(batch_inputs)
+            # print(attentions)
             inputs, labels = mask_tokens(batch_inputs, tokenizer, args) if args.mlm else (batch, batch)
             # debugging:
-            break
+            # break
             inputs = inputs.to(args.device)
             labels = labels.to(args.device)
             attentions = attentions.to(args.device)
