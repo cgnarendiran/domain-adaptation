0f74f2f843255e25db0d25a259b1003346d7d030
Narendiran Chembu <nchembu@test3.irc.sara.nl.irc.sara.nl>
2020-01-27 17:39:20 +0100

    updating job script with gpu patitions and correct syntaxes

--- domain-adaptation/finetuning_bert_enron/job.sh
+++ domain-adaptation/finetuning_bert_enron/job.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 #SBATCH --job-name=finetune_bert_enron
-#SBATCH --partition=gpu_short
-#SBATCH --time=5:00
+#SBATCH --partition=gpu
+#SBATCH --gres=gpu:1
+#SBATCH --time=2:00:00
 
 python ../run_lm_finetuning.py  \
     --output_dir='output/' \
     --model_type=bert \
     --model_name_or_path=bert-base-cased \
     --do_train \
-    --train_data_file='../enron_lm.csv' \
+    --train_data_file='../enron_lm.tsv' \
     --mlm \
     --overwrite_cache \
-    --overwrite_output_dir
+    --overwrite_output_dir \
+    --logging_steps=100 \
+    --per_gpu_train_batch_size=128
