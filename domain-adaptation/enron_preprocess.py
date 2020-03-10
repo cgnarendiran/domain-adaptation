89bf304bc49e0a9b0a2e6be9fbd2579b95751f62
Narendiran Chembu <cgnarendiran@gmail.com>
2020-03-10 15:39:55 +0100

    removing original quoted and forwarded text from enron emails

--- domain-adaptation/enron_preprocess.py
+++ domain-adaptation/enron_preprocess.py
@@ -10,41 +10,52 @@ import numpy as np
 
 def parse_doc(doc):
 	# finding the keys in this particular email 
-    keys = ['Message-ID']+re.findall('\n([\w\-]+):', doc[:doc.find('\n\n')])
-    keys = pd.Series(keys).drop_duplicates().tolist()
+	keys = ['Message-ID']+re.findall('\n([\w\-]+):', doc[:doc.find('\n\n')])
+	keys = pd.Series(keys).drop_duplicates().tolist()
 
-    values = []
-    for a, k in enumerate(keys):
-        k = k+':'
-        try:
-        	# append values for all the keys
-            values.append(doc[doc.find(k)+len(k):doc.find(keys[a+1])].strip())
-        except:
-        	# for the last key add values till the start of body which is '\n\n'
-            values.append(doc[doc.find(k)+len(k):doc.find('\n\n')].strip())
+	values = []
+	for a, k in enumerate(keys):
+		k = k+':'
+		try:
+			# append values for all the keys
+			values.append(doc[doc.find(k)+len(k):doc.find(keys[a+1])].strip())
+		except:
+			# for the last key add values till the start of body which is '\n\n'
+			values.append(doc[doc.find(k)+len(k):doc.find('\n\n')].strip())
 
-    # Remove entries that have less than 10 length and truncate the maximum length of emails to 127
-    body_value = doc[doc.find('\n\n'):].strip().strip('\n').strip('\n\n')  # stripping white spaces and new line chars 
-    body_length = len(body_value)
-    # uncomment this block to save with a threshold of body length
-    # if  body_length < 100:
-    # 	return None
-    # elif body_length >= 512:
-    # 	body_value = body_value[0:512]
+	# Remove entries that have less than 10 length and truncate the maximum length of emails to 127
+	og_message_pos =  doc.find('-----Original Message-----')
+	fw_message_pos =  doc.find('---------------------- Forwarded by')
+	if og_message_pos != -1:
+		# print("\nog_message_pos:", og_message_pos)
+		body_value = doc[doc.find('\n\n'):og_message_pos].strip() # stripping white spaces and new line chars
+	elif fw_message_pos != -1:
+		# print("\nfw_message_pos:", fw_message_pos)
+		body_value = doc[doc.find('\n\n'):fw_message_pos].strip()
+	else:
+		# print("\nno og or fw pos")
+		body_value = doc[doc.find('\n\n'):].strip()
 
-    # create dict of fields with values including the Body field
-    d = dict(zip(keys+['Body'],values+[body_value]))
+	body_length = len(body_value)
+	############ uncomment this block to save with a threshold of body length
+	if  body_length < 4:
+		return None
+	elif body_length >= 512:
+		body_value = body_value[0:512]
 
-    # add or remove keys from the exhaustive list of allkeys
-    k_to_remove = set(d.keys()) - set(allkeys)
-    k_to_add = set(allkeys) - set(d.keys())
-    
-    for k in k_to_remove:
-        d.pop(k)
-    for k in k_to_add:
-        d[k] = ''
-    # keys = [k[:-1] for k in keys]
-    return return_desired_fields(d, desired_fields)
+	# create dict of fields with values including the Body field
+	d = dict(zip(keys+['Body'],values+[body_value]))
+
+	# add or remove keys from the exhaustive list of allkeys
+	k_to_remove = set(d.keys()) - set(allkeys)
+	k_to_add = set(allkeys) - set(d.keys())
+	
+	for k in k_to_remove:
+		d.pop(k)
+	for k in k_to_add:
+		d[k] = ''
+	# keys = [k[:-1] for k in keys]
+	return return_desired_fields(d, desired_fields)
 
 
 def return_desired_fields(dict_doc, desired_fields):
@@ -76,7 +87,7 @@ if __name__== "__main__" :
 
 	# print(df.head())
 	# Looks like this:
-	                            # file                                            message
+								# file                                            message
 	# 8820   badeer-r/all_documents/264.  Message-ID: <11348414.1075863593218.JavaMail.e...
 	# 841     allen-p/all_documents/318.  Message-ID: <12293216.1075855672454.JavaMail.e...
 	# 32937           benson-r/inbox/31.  Message-ID: <19387609.1075840372845.JavaMail.e...
@@ -90,7 +101,7 @@ if __name__== "__main__" :
 	# parse the text fields:#
 	#########################
 	# finding all the existing keys from 50 sampled emails
-	docs = df.message.sample(50)
+	docs = df.message.sample(20)
 	allkeys = [re.findall('\n([\w\-]+):', doc[:doc.find('\n\n')]) for doc in docs]
 	allkeys = set(sum(allkeys,[]))
 	allkeys.add('Message-ID')
@@ -108,8 +119,15 @@ if __name__== "__main__" :
 
 	parsed_df = pd.DataFrame(parsed_list)
 	pp.pprint(parsed_df.head())
+	########################
+	# check alert!!
+	# open('debug_enron.txt', "w").write('\nNEW EMAIL\n'.join(parsed_df.loc[0:20,:].Body.values))
+	######################
+
+
+
 	# adding new lines as delimiters between examples and storing them as text files
-	open('enron_lm.txt', "w").write(''.join(parsed_df.Body.values))
+	open('enron_lm.txt', "w").write('\n'.join(parsed_df.Body.values))
 
 	# save the parsed dataframe to a file for training
 	# parsed_df.to_csv('enron_lm.tsv',sep='\t')
@@ -127,12 +145,15 @@ if __name__== "__main__" :
 	parsed_df_eval = pd.DataFrame(parsed_list)
 	# pp.pprint(parsed_df.head())
 	# adding new lines as delimiters between examples and storing them as text files
-	open('enron_lm_eval.txt', "w").write(''.join(parsed_df_eval.Body.values))
+	open('enron_lm_eval.txt', "w").write('\n'.join(parsed_df_eval.Body.values))
 
 	# save the parsed dataframe to a file for training
 	# parsed_df.to_csv('enron_lm.tsv',sep='\t')
 	# save a txt file when threholding of the body is NOT done:
 	# parsed_df.to_csv('enron_lm.txt', sep='\t', index=False, header=False)
+	
+
+
 	##################################################
 	# Checking the maximum body length of the emails:#
 	##################################################
